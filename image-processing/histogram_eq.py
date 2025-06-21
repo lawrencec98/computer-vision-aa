875b41a475784e40fe85204e34c2ec2045282d03
Lawrence <lawrencemcatapang@gmail.com>
2025-06-21 21:49:51 +0100

    Started on histogram equalization problem

---
+++ image-processing/histogram_eq.py
@@ -0,0 +1,38 @@
+# Exercise 3.7 Histogram equalization
+
+# Compute the gray level (luminance) histogram for an image and equalize it so that the tones look
+# better (and the image is less sensitive to exposure settings).
+
+import numpy as np
+import cv2 as cv
+import matplotlib.pyplot as plt
+
+
+def bgr2luminance(bgr_image):
+    # Luminance can be perceived from RGB by the following equation:
+    # 0.2126R + 0.7152G + 0.0722B
+
+    height, width, channels = bgr_image.shape
+    lum_img = np.zeros((height,width), dtype=np.uint8)
+    
+    for row in range(height):
+         for col in range(width):
+            bval = bgr_image[row][col][0]
+            gval = bgr_image[row][col][1]
+            rval = bgr_image[row][col][2]
+
+            lum_img[row][col] = (0.0722*bval) + (0.7152*gval) + (0.2126*rval)
+
+    return lum_img
+
+
+
+# Read in an image
+rgb_img = cv.imread('rgb_parrot.jpg')
+
+# print(mono_img)
+luminance_image = bgr2luminance(rgb_img)
+
+cv.imshow('before',rgb_img)
+cv.imshow('after', luminance_image)
+cv.waitKey(0)
---
+++ image-processing/histogram_plotter.py
@@ -0,0 +1,53 @@
+# Exercise 3.7 Histogram equalization
+
+# Compute the gray level (luminance) histogram for an image and equalize it so that the tones look
+# better (and the image is less sensitive to exposure settings).
+
+import numpy as np
+import cv2 as cv
+import matplotlib.pyplot as plt
+
+
+###############################
+# Step 1: Convert the color image to luminance.
+
+# Read image
+rgb_img = cv.imread('rgb_parrot.jpg')
+temp = cv.cvtColor(rgb_img, cv.COLOR_BGR2GRAY) # Convert from BGR to Grayscale
+
+mono_img = temp.copy()
+
+# Display images
+# cv.imshow('before', rgb_img)
+# cv.imshow('after', mono_img)
+# if cv.waitKey(0) == 'q':
+# 	cv.destroyAllWindows()
+
+
+###############################
+# Step 2: Compute the histogram, the cumulative distribution, and the compensation transfer function
+
+intensity_values = np.array(0)
+
+for row in mono_img:
+	for pixel in row:
+		intensity_values = np.append(intensity_values,pixel)
+
+
+# Plot histogram and cumulative distribution
+fig, ax1 = plt.subplots()
+
+counts, bins, patches = ax1.hist(intensity_values, bins=260, density=False, alpha=0.75, label='Histogram')
+ax1.set_xlabel('Pixel Intensity')
+ax1.set_ylabel('Count')
+ax1.legend(loc='upper left')
+
+# Cumulative on second y-axis
+ax2 = ax1.twinx()
+ax2.hist(intensity_values, bins=260, density=True, histtype='step', cumulative=True, color='r', label='Cumulative')
+ax2.set_ylabel('Cumulative Density')
+ax2.legend(loc='upper right')
+
+plt.title("Histogram and Cumulative Distribution")
+plt.grid(True)
+plt.show()
